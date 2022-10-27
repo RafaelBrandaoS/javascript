f878fde64119f17f6bb2ca802a47fa6ff0f2db32
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2022-10-27 14:03:53 -0300

    atualizei os exercícios do curso de JavaScript

---
+++ exercicios/ex008/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é ${vel}KM/H`)
+
+if (vel > 60) { //Condição simples!
+    console.log(`Voçê utrapaçou o limite de velocidade. Multado!`)
+}
+
+console.log(`Use sempre o sinto de segurança`)
---
+++ exercicios/ex009/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+
+console.groupCollapsed(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
