fb5ef259b58fd1cd848b53441fd7e8db25fb5803
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2022-10-31 18:17:30 -0300

    atualizei exercícios de javascript

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 5 && hora < 12) {
+        //BOM DIA!
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#fed5a0'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#bcdbf0'
+    } else {
+        //BOA NOITE!
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#266481'
+    }
+}
