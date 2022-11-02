4564699780e9b199194fb8371e703879e97e9139
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2022-11-02 12:59:31 -0300

    atualizei exercícios de javascript

--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -1,1 +1,50 @@
-alert('Olá!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.querySelector('input#txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'bebe-homem.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-homem.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulto-homem.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-homem.png')
+            }
+        } else {
+            genero = 'mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'bebe-mulher.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-mulher.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulto-mulher.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-mulher.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+
+}
