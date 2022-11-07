b2b173de4eb18197863a21b91242162652bc8b5c
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2022-11-07 16:22:34 -0300

    atualizei desafios

--- desafios/d004/script.js
+++ desafios/d004/script.js
@@ -1,21 +1,16 @@
-/*
 function tabuada() {
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('seltab')
+    let num = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
 
     if (num.value.length == 0) {
-        window.alert('Por favor, insira um numero!')
+        window.alert('Impossivel calcular')
     } else {
         let n = Number(num.value)
-        let c = 1 
         tab.innerHTML = ''
-        while (c <= 10) {
+        for (let c = 1; c <= 10; c++) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             tab.appendChild(item)
-            c++
         }
     }
 }
-*/
-
